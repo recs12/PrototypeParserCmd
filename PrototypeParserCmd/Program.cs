b109c9b06a24c14f08571d4bf84c52549718a115
slimane rechdi <slimane.rechdi@gmail.com>
2021-03-15 11:11:42 -0400

    all variables set

--- PrototypeParserCmd/Program.cs
+++ PrototypeParserCmd/Program.cs
@@ -7,11 +7,49 @@ namespace QuickStart
     {
         public class Options
         {
-            [Option('t', "teamcenter", Required = false, HelpText = "Set macro on teamcenter mode.")]
-            public bool Teamcenter { get; set; }
 
-            [Option("name", Required = false, HelpText = "user name.")]
-            public string name { get; set; }
+            // both modes options
+
+            [Option('m', "mode", Required = true, HelpText = "Set the macro mode SE/TC")]
+            public string Mode { get; set; }
+
+            [Option('o', "folder-target", Required = false, HelpText = "Directory where all the cads will be downloaded.")]
+            public string FolderTarget { get; set; }
+
+
+            // SE Mode options
+            [Option('f', "folder-source", Required = false, HelpText = "Directory containing all the cads.")]
+            public string FolderSource { get; set; }
+
+            [Option('j', "file-source", Required = false, HelpText = "Name of the file")]
+            public string FileSource { get; set; }
+
+            [Option('q', "file-target", Required = false, HelpText = "Filename as downloaded in the target directory.")]
+            public string FileTarget { get; set; }
+
+
+            // TC Mode options
+            [Option('i', "item-number", Required = false, HelpText = "Filename (without extension type.)")]
+            public string ItemNumber { get; set; }
+
+            [Option('v', "revision", Required = false, HelpText = "Revision of the cad.")]
+            public string Revision { get; set; }
+
+            [Option('u', "user", Required = false, HelpText = "User.")]
+            public string User { get; set; }
+
+            [Option('p', "password", Required = false, HelpText = "Password.")]
+            public string Password { get; set; }
+
+            [Option('g', "group", Required = false, HelpText = "Group.")]
+            public string Group { get; set; }
+
+            [Option('r', "role", Required = false, HelpText = "Role.")]
+            public string Role { get; set; }
+
+            [Option('s', "server", Required = false, HelpText = "Server.")]
+            public string Server { get; set; }
+
         }
 
         static void Main(string[] args)
@@ -19,21 +57,22 @@ namespace QuickStart
             Parser.Default.ParseArguments<Options>(args)
                    .WithParsed<Options>(o =>
                    {
-                       if (o.Teamcenter)
+                       if (o.Mode == "TC")
                        {
-                           Console.WriteLine($"Tc enabled : --name {o.name}");
-                           Console.WriteLine("Quick Start Example! App is in TC mode!");
-                           Console.WriteLine($"user: {o.name}");
+                           Console.WriteLine("App is in TC mode");
+                           Console.WriteLine($"Tc enabled : --folder-source {o.FolderSource}");
+                           Console.WriteLine($"user: {o.FolderSource}");
                            Console.ReadLine();
                        }
                        else
                        {
-                           Console.WriteLine("Quick Start Example! App is in SE mode!");
-                           Console.WriteLine($"Current Arguments: --name {o.name}");
+                           Console.WriteLine("App is in SE mode");
+                           Console.WriteLine($"Current Arguments: --folder-source {o.FolderSource}");
                            Console.WriteLine("Quick Start Example!");
                            Console.ReadLine();
                        }
-                   });
+                   }
+              );
         }
     }
 }
